7c01e5d7448d2db9409de3e741af2ceae805bafb
yugo-ibuki <y.ibuki91@gmail.com>
2022-11-03 23:28:49 +0900

    prisma data

---
+++ src/script.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const newLink = await prisma.link.create({
+    data: {
+      description: 'Fullstack tutorial for GraphQL',
+      url: 'www.howtographql.com'
+    }
+  })
+  const allLinks = await prisma.link.findMany()
+  console.log(allLinks)
+}
+
+main()
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
