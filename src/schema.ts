03fac8b941f7be9df5d224e3a7f9a3739c03d71e
yugo-ibuki <y.ibuki91@gmail.com>
2022-11-04 00:49:57 +0900

    add error handling

--- src/schema.ts
+++ src/schema.ts
@@ -1,6 +1,15 @@
 import { makeExecutableSchema } from '@graphql-tools/schema'
 import type { GraphQLContext } from './context'
+import { GraphQLError } from 'graphql'
 import type { Link } from '@prisma/client'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+
+const parseIntSafe = (value: string): number | null => {
+  if (/^(\d+)$/.test(value)) {
+    return parseInt(value, 10)
+  }
+  return null
+}
 
 export const typeDefinitions = /* GraphQL */ `
   type Query {
@@ -84,14 +93,36 @@ const resolvers = {
       args: { linkId: string; body: string },
       context: GraphQLContext
     ) {
-      const newComment = await context.prisma.comment.create({
-        data: {
-          linkId: parseInt(args.linkId),
-          body: args.body
-        }
-      })
+      const linkId = parseIntSafe(args.linkId)
+      if (linkId === null) {
+        return Promise.reject(
+          new GraphQLError(
+            `Cannot post comment on non-existing link with id '${args.linkId}'.`
+          )
+        )
+      }
 
-      return newComment
+      try {
+        const newComment = await context.prisma.comment.create({
+          data: {
+            linkId: parseInt(args.linkId),
+            body: args.body
+          }
+        })
+        return newComment
+      } catch(err: unknown) {
+        if (
+          err instanceof PrismaClientKnownRequestError &&
+          err.code === 'P2003'
+        ) {
+          return Promise.reject(
+            new GraphQLError(
+              `Cannot post comment on non-existing link with id '${args.linkId}'.`
+            )
+          )
+        }
+        return Promise.reject(err)
+      }
     }
   }
 }
